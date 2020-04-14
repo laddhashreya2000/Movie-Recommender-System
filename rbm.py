bf0c57e1d1fb5b7661a460ba439ad21b1e5d995f
Shreya Laddha <47536876+laddhashreya2000@users.noreply.github.com>
2020-04-15 00:05:13 +0530

    Update rbm.py

--- rbm.py
+++ rbm.py
@@ -10,19 +10,12 @@ import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
 
-# Importing the dataset
-movies = pd.read_csv('ml-1m/movies.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
-users = pd.read_csv('ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
-ratings = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')#last column is the time stamp...not useful
-
-# Preparing the training set and the test set
-#u1 is the first split.....we hsve five sets
+#u1 is the first split.....we have five sets
 training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
 training_set = np.array(training_set, dtype = 'int')
 test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
 test_set = np.array(test_set, dtype = 'int')
 
-# Getting the number of users and movies
 nb_users = int(max(max(training_set[:,0]), max(test_set[:,0])))
 nb_movies = int(max(max(training_set[:,1]), max(test_set[:,1])))
 
@@ -41,7 +34,6 @@ def convert(data):
 training_set = convert(training_set)
 test_set = convert(test_set)
 
-# Converting the data into Torch tensors
 training_set = torch.FloatTensor(training_set)
 test_set = torch.FloatTensor(test_set)
 
