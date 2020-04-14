705ae7d4d44a7c9dfe571ccf6823aa654be76398
Shreya Laddha <47536876+laddhashreya2000@users.noreply.github.com>
2020-04-15 00:04:00 +0530

    Update ae.py

--- ae.py
+++ ae.py
@@ -10,18 +10,11 @@ import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
 
-# Importing the dataset
-movies = pd.read_csv('ml-1m/movies.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
-users = pd.read_csv('ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
-ratings = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
-
-# Preparing the training set and the test set
 training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
 training_set = np.array(training_set, dtype = 'int')
 test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
 test_set = np.array(test_set, dtype = 'int')
 
-# Getting the number of users and movies
 nb_users = int(max(max(training_set[:,0]), max(test_set[:,0])))
 nb_movies = int(max(max(training_set[:,1]), max(test_set[:,1])))
 
@@ -38,7 +31,6 @@ def convert(data):
 training_set = convert(training_set)
 test_set = convert(test_set)
 
-# Converting the data into Torch tensors
 training_set = torch.FloatTensor(training_set)
 test_set = torch.FloatTensor(test_set)
 
@@ -81,7 +73,6 @@ for epoch in range(1, nb_epoch + 1):
             optimizer.step()#backward decides the direction(increase/decrease), optimizer determines the intensity
     print('epoch: '+str(epoch)+' loss: '+str(train_loss/s))
 
-# Testing the SAE
 test_loss = 0
 s = 0.
 for id_user in range(nb_users):
